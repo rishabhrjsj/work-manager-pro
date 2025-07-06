4ea0fca95fb168b52899b712c0a548f15e1b2f3a
Rishabh Jaiswal <rishabhrjjaiwal@gmail.com>
2025-07-06 16:22:35 +0530

    error resolve:

--- work-management/src/app/helper/axios.js
+++ work-management/src/app/helper/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpAxios = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || "", // "" works for same-origin in Next.js
+  baseURL: process.env.BASE_URL || "", // "" works for same-origin in Next.js
   withCredentials: true, // ✅ ensures cookies like loginToken are sent
 });
 
